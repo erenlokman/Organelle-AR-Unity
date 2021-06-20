128df095eb193e0d4a6607d5a154e2ae19e8c7cd
erenlokman <37442138+erenlokman@users.noreply.github.com>
2021-06-20 20:59:09 +0300

    'organelleAR'

---
+++ Assets/Scripts/scaleControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scaleControl : MonoBehaviour
+{
+
+    public GameObject ARSessionToScale;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ScaleUp() 
+    {
+        Vector3 OScale = ARSessionToScale.transform.localScale;
+        Vector3 NScale = OScale * 0.9f;
+        ARSessionToScale.transform.localScale = NScale;
+    }
+
+    public void ScaleDown()
+    {
+        Vector3 OScale = ARSessionToScale.transform.localScale;
+        Vector3 NScale = OScale * 1.1f;
+        ARSessionToScale.transform.localScale = NScale;
+
+    }
+}
